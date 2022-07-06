e709c13543b3617844c4a775f859ce40522788d9
White Nguyen <whitenguyen0909@gmail.com>
2022-07-06 10:56:45 -0600

    copied the recursive solution

--- src/reverse/reverse.js
+++ src/reverse/reverse.js
@@ -14,8 +14,8 @@ function reverseIterative(list) {
   //declare a variable named reversedList and initialize it to an empty linked list
   //declare a variable node initialized to the head of the list
   //while the node pointer is not null do
-    //insert the node value at the head of the reversedList
-    //move the node pointer along to the next node
+  //insert the node value at the head of the reversedList
+  //move the node pointer along to the next node
   //return the reversedList
   const reversedList = new LinkedList();
   let node = list.head;
@@ -37,6 +37,23 @@ function reverseIterative(list) {
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseRecursive(list) {}
+function reverseRecursive(list) {
+  //base case
+  if (list.length <= 1) {
+    return list;
+  }
+  //recursive case
+  const head = list.head.value;
+  list.remove((node, index) => index === 0);
+  //going through the list's nodes all the way to the end
+  const reversedList = reverseRecursive(list);
+
+  //backward phase;
+  reversedList.insert(head);
+
+  return reversedList;
+}
+const expected = new LinkedList([5, 4, 3, 2, 1]);
+const actual = reverseRecursive(expected);
 
 module.exports = { reverseIterative, reverseRecursive };
