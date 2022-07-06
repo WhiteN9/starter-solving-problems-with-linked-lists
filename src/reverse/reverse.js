baaf8f96a012a6ee9209344c65aa2693603e0825
White Nguyen <whitenguyen0909@gmail.com>
2022-07-06 10:03:50 -0600

    added reverse iterative for linked list

--- src/reverse/reverse.js
+++ src/reverse/reverse.js
@@ -9,7 +9,19 @@ const LinkedList = require("../lib/linkedList");
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseIterative(list) {}
+function reverseIterative(list) {
+  const newList = new LinkedList();
+
+  let node = list.head;
+  while (node !== null) {
+    newList.insertAtHead(node.value);
+    node = node.next;
+  }
+  return newList;
+}
+
+const expected = new LinkedList([5, 4, 3, 2, 1]);
+const actual = reverseIterative(expected);
 
 /**
  * Recursively reverse a linked list
